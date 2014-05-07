70a1ece4f2e4ec9bad643bd7daa94dae6b0b797b
kelson42 <kelson@kiwix.org>
2014-05-07 18:05:44 +0200

    + Encode embeded fonts in inline CSS (because same-origin security blocks them otherwise)

--- zimwriterfs/zimwriterfs.cpp
+++ zimwriterfs/zimwriterfs.cpp
@@ -89,6 +89,56 @@ inline bool fileExists(const std::string &path) {
   return flag;
 }
 
+/* base64 */
+static const std::string base64_chars = 
+             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+             "abcdefghijklmnopqrstuvwxyz"
+  "0123456789+/";
+
+std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
+  std::string ret;
+  int i = 0;
+  int j = 0;
+  unsigned char char_array_3[3];
+  unsigned char char_array_4[4];
+
+  while (in_len--) {
+    char_array_3[i++] = *(bytes_to_encode++);
+    if (i == 3) {
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+      char_array_4[3] = char_array_3[2] & 0x3f;
+
+      for(i = 0; (i <4) ; i++)
+        ret += base64_chars[char_array_4[i]];
+      i = 0;
+    }
+  }
+
+  if (i)
+    {
+      for(j = i; j < 3; j++)
+	char_array_3[j] = '\0';
+
+      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+      char_array_4[3] = char_array_3[2] & 0x3f;
+
+      for (j = 0; (j < i + 1); j++)
+	ret += base64_chars[char_array_4[j]];
+
+      while((i++ < 3))
+	ret += '=';
+
+    }
+
+  return ret;
+
+}
+
+
 inline std::string decodeUrl(const std::string &encodedUrl) {
   std::string decodedUrl = encodedUrl;
   std::string::size_type pos = 0;
@@ -690,7 +740,18 @@ zim::Blob ArticleSource::getData(const std::string& aid) {
 	url = css.substr(startPos, endPos - startPos);
 	
 	if (url.substr(0, 5) != "data:") {
-	  replaceStringInPlace(css, url, computeNewUrl(aid, url));
+	  std::string mimeType = getMimeTypeForFile(url);
+	  
+	  /* Embeded fonts need to be inline because Kiwix is
+	     otherwise not able to load same because of the
+	     same-origin security */
+	  if (mimeType == "application/x-font-ttf" ||  mimeType == "application/vnd.ms-opentype") {
+	    std::string fontPath = directoryPath + "/" + computeAbsolutePath(aid, url);
+	    std::string fontContent = getFileContent(fontPath);
+	    replaceStringInPlace(css, url, "data:" + mimeType + ";base64," + base64_encode(reinterpret_cast<const unsigned char*>(fontContent.c_str()), fontContent.length()));
+	  } else {
+	    replaceStringInPlace(css, url, computeNewUrl(aid, url));
+	  }
 	}
       }
 
